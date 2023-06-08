964b70efbcb5a2fe4cd9bee391ca54867130d851
zhouxl <zhouxl@spacemit.com>
2023-06-08 12:11:07 +0800

    add zicbom extention for uboot, support for non-coherent DMA operation
    Change-Id: I4cc61a805ec0041201292c6ce03e231ef4845b4e

--- arch/riscv/cpu/baize/cpu.c
+++ arch/riscv/cpu/baize/cpu.c
@@ -21,3 +21,45 @@ int cleanup_before_linux(void)
 
 	return 0;
 }
+
+#if CONFIG_IS_ENABLED(RISCV_ISA_ZICBOM)
+int check_cache_range(unsigned long start, unsigned long end)
+{
+	int ok = 1;
+
+	if (start & (CONFIG_RISCV_CBOM_BLOCK_SIZE - 1))
+		ok = 0;
+
+	if (end & (CONFIG_RISCV_CBOM_BLOCK_SIZE - 1))
+		ok = 0;
+
+	if (!ok) {
+		warn_non_spl("CACHE: Misaligned operation at range [%08lx, %08lx]\n",
+			start, end);
+	}
+
+	return ok;
+}
+
+void invalidate_dcache_range(unsigned long start, unsigned long end)
+{
+	if (!check_cache_range(start, end))
+		return;
+
+	while (start < end) {
+		cbo_invalid(start);
+		start += CONFIG_RISCV_CBOM_BLOCK_SIZE;
+	}
+}
+
+void flush_dcache_range(unsigned long start, unsigned long end)
+{
+	if (!check_cache_range(start, end))
+		return;
+
+	while (start < end) {
+		cbo_flush(start);
+		start += CONFIG_RISCV_CBOM_BLOCK_SIZE;
+	}
+}
+#endif
