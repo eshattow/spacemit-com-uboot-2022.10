d4c18205af9fe3d8a36d300b6a2fe47433cdd81c
zhangmeng <zhangmeng.kevin@space-t.cn>
2023-06-10 15:47:53 +0800

    [update] rename name of cpu core from baize to x60
    Change-Id: I38377d147ac5936f1e7a04b9128b352c7054431e

--- board/spacemit/k1-pro/k1pro.c
+++ board/spacemit/k1-pro/k1pro.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0+
 /*
- * Copyright (C) 2018, Bin Meng <bmeng.cn@gmail.com>
+ * Copyright (C) 2023, kevin.z.m <zhangmeng.kevin@spacemit.com>
  */
 
 #include <common.h>
