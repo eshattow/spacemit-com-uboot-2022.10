f79a828c2d2daa8abacf967917033442524140d1
zhangmeng <zhangmeng.kevin@spacemit.com>
2023-08-09 09:07:10 +0800

    [k1x/plat] update k1x platform configuration
    Change-Id: I11880dd84cbaa1b90f575ed6b124741945bd0682

--- include/configs/k1-x.h
+++ include/configs/k1-x.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0+ */
 /*
- * Copyright (C) 2022, Kevin.z.m <zhangmeng.kevin@spacemit.com>
+ * Copyright (C) 2023, Spacemit
  */
 
 #ifndef __CONFIG_H
@@ -8,49 +8,50 @@
 
 #include <linux/sizes.h>
 
+#define SYS_DRAM_OFFS               0x0000000
+#define SZ_2GB                      0x80000000
+#define SZ_4GB                      0x100000000ULL
+#define SEC_IMG_SIZE                0x2000000
+#define CONFIG_SYS_SDRAM_BASE       (SYS_DRAM_OFFS + SEC_IMG_SIZE)
 
-#if defined(CONFIG_K1_X_BOARD_FPGA) || defined(CONFIG_K1_X_BOARD_SIMULATION)
-    #define RISCV_MMODE_TIMERBASE		0x2000000
-    #define RISCV_MMODE_TIMER_FREQ		1000000
-    #define RISCV_SMODE_TIMER_FREQ		1000000
-#else
-    #error "unknown k1-x board defined"
-#endif
+#define RISCV_MMODE_TIMERBASE       0xE4000000
+#define RISCV_MMODE_TIMER_FREQ      1000000
+#define RISCV_SMODE_TIMER_FREQ      1000000
 
-#define CONFIG_IPADDR    10.0.92.253
-#define CONFIG_SERVERIP  10.0.92.134
-#define CONFIG_GATEWAYIP 10.0.92.1
-#define CONFIG_NETMASK   255.255.255.0
+#define CONFIG_IPADDR               10.0.92.253
+#define CONFIG_SERVERIP             10.0.92.134
+#define CONFIG_GATEWAYIP            10.0.92.1
+#define CONFIG_NETMASK              255.255.255.0
 
-#define K1X_SPL_BOOT_LOAD_ADDR  (0x00000000)
+#define K1X_SPL_BOOT_LOAD_ADDR      (0x20200000)
 
 /* Environment options */
 
 #define BOOT_TARGET_DEVICES(func) \
-	func(QEMU, qemu, na)
+    func(QEMU, qemu, na)
 
 #include <config_distro_bootcmd.h>
 
 #define BOOTENV_DEV_QEMU(devtypeu, devtypel, instance) \
-	"bootcmd_qemu=" \
-		"if env exists kernel_start; then " \
-			"bootm ${kernel_start} - ${fdtcontroladdr};" \
-		"fi;\0"
+    "bootcmd_qemu=" \
+    "if env exists kernel_start; then " \
+    "bootm ${kernel_start} - ${fdtcontroladdr};" \
+    "fi;\0"
 
 #define BOOTENV_DEV_NAME_QEMU(devtypeu, devtypel, instance) \
-	"qemu "
+    "qemu "
 
 #define CONFIG_EXTRA_ENV_SETTINGS \
-	"fdt_high=0xffffffffffffffff\0" \
-	"initrd_high=0xffffffffffffffff\0" \
-	"kernel_addr_r=0x84000000\0" \
-	"kernel_comp_addr_r=0x88000000\0" \
-	"kernel_comp_size=0x4000000\0" \
-	"fdt_addr_r=0x8c000000\0" \
-	"scriptaddr=0x8c100000\0" \
-	"pxefile_addr_r=0x8c200000\0" \
-	"ramdisk_addr_r=0x8c300000\0" \
-	"ethaddr=02:f6:c3:67:27:55\0" \
-	BOOTENV
+    "fdt_high=0xffffffffffffffff\0" \
+    "initrd_high=0xffffffffffffffff\0" \
+    "kernel_addr_r=0x24000000\0" \
+    "kernel_comp_addr_r=0x28000000\0" \
+    "kernel_comp_size=0x4000000\0" \
+    "fdt_addr_r=0x2c000000\0" \
+    "scriptaddr=0x2c100000\0" \
+    "pxefile_addr_r=0x2c200000\0" \
+    "ramdisk_addr_r=0x2c300000\0" \
+    "ethaddr=02:f6:c3:67:27:55\0" \
+    BOOTENV
 
 #endif /* __CONFIG_H */
