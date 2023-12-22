c7c4f0a3b16b5783b0f383ebef03e8ab9b64aa9b
wanlong <long.wan@spacemit.com>
2023-12-22 15:20:12 +0800

    mmc: k1x_sdhci: support mmc hs400
    Change-Id: Iab236b803df4bdc478f464296eb660a59721e7f3

--- drivers/mmc/k1x_sdhci.c
+++ drivers/mmc/k1x_sdhci.c
@@ -56,7 +56,12 @@ DECLARE_GLOBAL_DATA_PTR;
 #define SDHC_PHY_DLLCFG1		0x16C
 #define DLL_REG2_CTRL			0x0C
 #define DLL_REG3_CTRL_MASK		0xFF
-#define DLL_REG3_CTRL_SHIFT		0x08
+#define DLL_REG3_CTRL_SHIFT		0x10
+#define DLL_REG2_CTRL_MASK		0xFF
+#define DLL_REG2_CTRL_SHIFT		0x08
+#define DLL_REG1_CTRL			0x92
+#define DLL_REG1_CTRL_MASK		0xFF
+#define DLL_REG1_CTRL_SHIFT		0x00
 
 #define SDHC_PHY_DLLSTS			0x170
 #define DLL_LOCK_STATE			0x01
@@ -64,6 +69,12 @@ DECLARE_GLOBAL_DATA_PTR;
 #define SDHC_PHY_DLLSTS1		0x174
 #define DLL_MASTER_DELAY_MASK		0xFF
 #define DLL_MASTER_DELAY_SHIFT		0x10
+
+#define SDHC_PHY_PADCFG_REG		0x178
+#define RX_BIAS_CTRL_SHIFT		0x5
+#define PHY_DRIVE_SEL_SHIFT		0x0
+#define PHY_DRIVE_SEL_MASK		0x7
+#define PHY_DRIVE_SEL_DEFAULT		0x4
 
 #define RPM_DELAY			50
 #define MAX_74CLK_WAIT_COUNT		74
@@ -110,16 +121,22 @@ struct spacemit_sdhci_plat {
 
 struct spacemit_sdhci_priv {
 	struct sdhci_host host;
+	u32 clk_src_freq;
 	u32 phy_module;
+};
+
+struct spacemit_sdhci_driver_data {
+	const struct sdhci_ops *ops;
+	u32 flags;
 };
 
 /*
  * refer to PMU_SDH0_CLK_RES_CTRL<0x054>, SDH0_CLK_SEL:0x0, SDH0_CLK_DIV:0x1
- * the default clock source is 200MHz [400MHz(pll1_400Mhz)/2]
+ * the default clock source is 204800000Hz [409.6MHz(pll1_d6_409p6Mhz)/2]
  *
  * in the start-up phase, use the 200KHz frequency
  */
-#define SDHC_DEFAULT_MAX_CLOCK (200*1000*1000)
+#define SDHC_DEFAULT_MAX_CLOCK (204800000)
 #define SDHC_MIN_CLOCK (200*1000)
 
 static int is_emulator_platform(void)
@@ -131,32 +148,106 @@ static int is_emulator_platform(void)
 #endif
 }
 
-static void set_emmc_phy_bypass(struct sdhci_host *host)
+static int spacemit_reg[] = {
+	0x100, 0x104, 0x108, 0x10c, 0x110, 0x114, 0x118, 0x11c,
+	0x120, 0x124, 0x128, 0x12c, 0x130, 0x134, 0x160, 0x164,
+	0x168, 0x16c, 0x170, 0x174, 0x178, 0x17c, 0x180, 0x184,
+	0x188, 0x18c, 0x190, 0x1f0, 0x1f4, 0xFFF,
+};
+static u8 cur_com_reg[960]; /* 8 line, 120  character  per line */
+static u8 cur_pri_reg[960];
+
+static void __maybe_unused dump_sdh_regs(struct sdhci_host *host, u8 is_emmc)
 {
+	int val;
+	int offset;
+	int i;
+	int len;
+	char *buf;
+
+	buf = (char *)&cur_com_reg[0];
+	len = 0;
+	i = 0;
+	for (offset = 0; offset < 0x70; offset += 4) {
+		val = sdhci_readl(host, offset);
+		if (i % 4 == 0)
+			len += sprintf(buf + len, "\n");
+		len += sprintf(buf + len, "\toffset:0x%03x 0x%08x\t", offset, val);
+		i++;
+	}
+
+	if (i % 4 == 0)
+		len += sprintf(buf + len, "\n");
+	val = sdhci_readl(host, 0xe0);
+	len += sprintf(buf + len, "\toffset:0x%03x 0x%08x\t", 0xe0, val);
+	val = sdhci_readl(host, 0xfc);
+	len += sprintf(buf + len, "\toffset:0x%03x 0x%08x\t\n", 0xfc, val);
+
+	buf = (char *)&cur_pri_reg[0];
+	len = 0;
+	i = 0;
+	do {
+		if (!is_emmc && (spacemit_reg[i] > 0x134))
+			break;
+		val = sdhci_readl(host, spacemit_reg[i]);
+		if (i % 4 == 0)
+			len += sprintf(buf + len, "\n");
+		len += sprintf(buf + len, "\toffset:0x%03x 0x%08x\t", spacemit_reg[i], val);
+		i++;
+	} while (spacemit_reg[i] != 0xFFF);
+	len += sprintf(buf + len, "\n");
+
+	printf("%s", cur_com_reg);
+	printf("%s", cur_pri_reg);
+}
+
+static void spacemit_mmc_phy_init(struct sdhci_host *host)
+{
+	struct udevice *dev = host->mmc->dev;
+	struct spacemit_sdhci_priv *priv = dev_get_priv(dev);
 	unsigned int value = 0;
 
-	/* set emmc phy bypass if need */
-	if (is_emulator_platform()) {
-		/* phy bypass */
+	if (priv->phy_module) {
+		/* mmc card mode */
+		value = sdhci_readl(host, SDHC_MMC_CTRL_REG);
+		value |= MMC_CARD_MODE;
+		sdhci_writel(host, value, SDHC_MMC_CTRL_REG);
+
+		if (is_emulator_platform()) {
+			/* mmc phy bypass */
+			value = sdhci_readl(host, SDHC_TX_CFG_REG);
+			value |= TX_INT_CLK_SEL;
+			sdhci_writel (host, value, SDHC_TX_CFG_REG);
+
+			value = sdhci_readl(host, SDHC_PHY_CTRL_REG);
+			value |= HOST_LEGACY_MODE;
+			sdhci_writel (host, value, SDHC_PHY_CTRL_REG);
+
+			value = sdhci_readl(host, SDHC_PHY_FUNC_REG);
+			value |= PHY_TEST_EN;
+			sdhci_writel (host, value, SDHC_PHY_FUNC_REG);
+			printf("%s: mmc phy bypass.\n", host->name);
+		} else {
+			/* use phy func mode */
+			value = sdhci_readl(host, SDHC_PHY_CTRL_REG);
+			value |= (PHY_FUNC_EN | PHY_PLL_LOCK);
+			sdhci_writel(host, value, SDHC_PHY_CTRL_REG);
+
+			value = sdhci_readl(host, SDHC_PHY_PADCFG_REG);
+			value |= (1 << RX_BIAS_CTRL_SHIFT);
+			sdhci_writel(host, value, SDHC_PHY_PADCFG_REG);
+			printf("%s: use mmc phy func.\n", host->name);
+		}
+	} else {
+		printf("%s: not support phy module.\n", host->name);
 		value = sdhci_readl (host, SDHC_TX_CFG_REG);
 		value |= TX_INT_CLK_SEL;
 		sdhci_writel (host, value, SDHC_TX_CFG_REG);
-
-		value = sdhci_readl (host, SDHC_PHY_CTRL_REG);
-		value |= HOST_LEGACY_MODE;
-		sdhci_writel (host, value, SDHC_PHY_CTRL_REG);
-
-		value = sdhci_readl (host, SDHC_PHY_FUNC_REG);
-		value |= PHY_TEST_EN;
-		sdhci_writel (host, value, SDHC_PHY_FUNC_REG);
-
-		printf("%s: emmc phy bypass.\n", host->name);
-	} else {
-		/* phy func mode */
-		value = sdhci_readl(host, SDHC_PHY_CTRL_REG);
-		value |= (PHY_FUNC_EN | PHY_PLL_LOCK);
-		sdhci_writel(host, value, SDHC_PHY_CTRL_REG);
 	}
+
+	value = sdhci_readl(host, SDHC_MMC_CTRL_REG);
+	value &= ~ENHANCE_STROBE_EN;
+	sdhci_writel(host, value, SDHC_MMC_CTRL_REG);
 }
 
 #define MAX_WAIT_COUNT 100
@@ -187,32 +278,114 @@ int spacemit_set_sdh_74_clk(struct udevice *dev)
 	return 0;
 }
 
+void spacemit_sdhci_set_control_reg(struct sdhci_host *host)
+{
+	struct mmc *mmc = (struct mmc *)host->mmc;
+	u32 reg;
+
+	if (mmc->signal_voltage == MMC_SIGNAL_VOLTAGE_180) {
+		reg = sdhci_readw(host, SDHCI_HOST_CONTROL2);
+		reg |= SDHCI_CTRL_VDD_180;
+		sdhci_writew(host, reg, SDHCI_HOST_CONTROL2);
+	}
+
+	if (mmc->selected_mode > SD_HS &&
+	    mmc->selected_mode <= MMC_HS_400_ES)
+		sdhci_set_uhs_timing(host);
+
+	/* according to the SDHC_TX_CFG_REG(0x11c<bit>),
+	 * set TX_INT_CLK_SEL to gurantee the hold time
+	 * at default speed mode or HS/SDR12/SDR25/SDR50 mode.
+	 */
+	reg = sdhci_readl(host, SDHC_TX_CFG_REG);
+	if ((mmc->selected_mode == MMC_LEGACY) ||
+	    (mmc->selected_mode == MMC_HS) ||
+	    (mmc->selected_mode == SD_HS) ||
+	    (mmc->selected_mode == UHS_SDR12) ||
+	    (mmc->selected_mode == UHS_SDR25) ||
+	    (mmc->selected_mode == UHS_SDR50)) {
+		reg |= TX_INT_CLK_SEL;
+	} else {
+		reg &= ~TX_INT_CLK_SEL;
+	}
+	sdhci_writel(host, reg, SDHC_TX_CFG_REG);
+
+	if ((mmc->selected_mode == MMC_HS_200) ||
+	    (mmc->selected_mode == MMC_HS_400) ||
+	    (mmc->selected_mode == MMC_HS_400_ES)) {
+		reg = sdhci_readw(host, SDHC_MMC_CTRL_REG);
+		reg |= (mmc->selected_mode == MMC_HS_200) ? MMC_HS200 : MMC_HS400;
+		sdhci_writew(host, reg, SDHC_MMC_CTRL_REG);
+	} else {
+		reg = sdhci_readw(host, SDHC_MMC_CTRL_REG);
+		reg &= ~(MMC_HS200 | MMC_HS400 | ENHANCE_STROBE_EN);
+		sdhci_writew(host, reg, SDHC_MMC_CTRL_REG);
+	}
+}
+
+#if CONFIG_IS_ENABLED(MMC_HS400_ES_SUPPORT)
+static int spacemit_sdhci_phy_dll_init(struct sdhci_host *host)
+{
+	u32 reg;
+	int i;
+
+	/* config dll_reg1 & dll_reg2 */
+	reg = sdhci_readl(host, SDHC_PHY_DLLCFG);
+	reg |= (DLL_PREDLY_NUM | DLL_FULLDLY_RANGE | DLL_VREG_CTRL);
+	sdhci_writel(host, reg, SDHC_PHY_DLLCFG);
+
+	reg = sdhci_readl(host, SDHC_PHY_DLLCFG1);
+	reg |= (DLL_REG1_CTRL & DLL_REG1_CTRL_MASK);
+	sdhci_writel(host, reg, SDHC_PHY_DLLCFG1);
+
+	/* dll enable */
+	reg = sdhci_readl(host, SDHC_PHY_DLLCFG);
+	reg |= DLL_ENABLE;
+	sdhci_writel(host, reg, SDHC_PHY_DLLCFG);
+
+	/* wait dll lock */
+	i = 0;
+	while (i++ < 100) {
+		if (sdhci_readl(host, SDHC_PHY_DLLSTS) & DLL_LOCK_STATE)
+			break;
+		udelay(10);
+	}
+	if (i == 100) {
+		printf("%s: phy dll lock timeout\n", host->name);
+		return 1;
+	}
+
+	return 0;
+}
+
+static int spacemit_sdhci_hs400_enhanced_strobe(struct sdhci_host *host)
+{
+	u32 reg;
+
+	reg = sdhci_readl(host, SDHC_MMC_CTRL_REG);
+	reg |= ENHANCE_STROBE_EN;
+	sdhci_writel(host, reg, SDHC_MMC_CTRL_REG);
+
+	return spacemit_sdhci_phy_dll_init(host);
+}
+#endif
+
 static int spacemit_sdhci_probe(struct udevice *dev)
 {
+	struct spacemit_sdhci_driver_data *drv_data =
+			(struct spacemit_sdhci_driver_data *)dev_get_driver_data(dev);
 	struct spacemit_sdhci_plat *plat = dev_get_plat(dev);
 	struct mmc_uclass_priv *upriv = dev_get_uclass_priv(dev);
 	struct spacemit_sdhci_priv *priv = dev_get_priv(dev);
 	struct sdhci_host *host = &priv->host;
 	struct dm_mmc_ops *mmc_driver_ops = (struct dm_mmc_ops *)dev->driver->ops;
-	u32 value;
+	struct clk clk;
 	int ret = 0;
 
 	host->mmc = &plat->mmc;
 	host->mmc->priv = host;
 	host->mmc->dev = dev;
 	upriv->mmc = host->mmc;
-
-	ret = reset_get_bulk(dev, &plat->resets);
-	if (ret) {
-		pr_err("Can't get reset: %d\n", ret);
-		return ret;
-	}
-
-	ret = reset_deassert_bulk(&plat->resets);
-	if (ret) {
-		pr_err("Failed to reset: %d\n", ret);
-		return ret;
-	}
 
 	ret = clk_get_bulk(dev, &plat->clks);
 	if (ret) {
@@ -226,17 +399,42 @@ static int spacemit_sdhci_probe(struct udevice *dev)
 		return ret;
 	}
 
+	ret = reset_get_bulk(dev, &plat->resets);
+	if (ret) {
+		pr_err("Can't get reset: %d\n", ret);
+		return ret;
+	}
+
+	ret = reset_deassert_bulk(&plat->resets);
+	if (ret) {
+		pr_err("Failed to reset: %d\n", ret);
+		return ret;
+	}
+
+	ret = clk_get_by_index(dev, 0, &clk);
+	if (ret) {
+		pr_err("Can't get io clk: %d\n", ret);
+		return ret;
+	}
+
+	ret = clk_set_rate(&clk, priv->clk_src_freq);
+	if (ret) {
+		pr_err("Failed to set io clk: %d\n", ret);
+		return ret;
+	}
+
 	/* Set quirks */
 	host->quirks = SDHCI_QUIRK_WAIT_SEND_CMD | SDHCI_QUIRK_32BIT_DMA_ADDR;
 	host->host_caps = MMC_MODE_HS | MMC_MODE_HS_52MHz;
-	host->max_clk = SDHC_DEFAULT_MAX_CLOCK;
+	host->max_clk = priv->clk_src_freq;
 
-	plat->cfg.f_max = SDHC_DEFAULT_MAX_CLOCK;
+	plat->cfg.f_max = priv->clk_src_freq;
 	plat->cfg.f_min = SDHC_MIN_CLOCK;
+	host->ops = drv_data->ops;
 
 	mmc_driver_ops->deferred_probe = spacemit_set_sdh_74_clk;
 
-	ret = sdhci_setup_cfg(&plat->cfg, host, SDHC_DEFAULT_MAX_CLOCK, SDHC_MIN_CLOCK);
+	ret = sdhci_setup_cfg(&plat->cfg, host, priv->clk_src_freq, SDHC_MIN_CLOCK);
 	if (ret)
 		return ret;
 
@@ -245,21 +443,13 @@ static int spacemit_sdhci_probe(struct udevice *dev)
 		return ret;
 
 	/* emmc phy bypass if need */
-	if (priv->phy_module) {
-		printf("%s: set phy module.\n", host->name);
-		set_emmc_phy_bypass(host);
-	} else {
-		printf("%s: not support phy module.\n", host->name);
-		value = sdhci_readl (host, SDHC_TX_CFG_REG);
-		value |= TX_INT_CLK_SEL;
-		sdhci_writel (host, value, SDHC_TX_CFG_REG);
-	}
+	spacemit_mmc_phy_init(host);
 
 	printf("%s: probe done.\n", host->name);
 	return ret;
 }
 
-static int spacemit_sdhci_ofdata_to_platdata(struct udevice *dev)
+static int spacemit_sdhci_of_to_plat(struct udevice *dev)
 {
 	struct spacemit_sdhci_plat *plat = dev_get_plat(dev);
 	struct spacemit_sdhci_priv *priv = dev_get_priv(dev);
@@ -272,9 +462,8 @@ static int spacemit_sdhci_ofdata_to_platdata(struct udevice *dev)
 	host->ioaddr = (void *)devfdt_get_addr(dev);
 	priv->phy_module = fdtdec_get_uint(blob, node, "sdh-phy-module", 0);
 
+	priv->clk_src_freq = fdtdec_get_uint(blob, node, "clk-src-freq", SDHC_DEFAULT_MAX_CLOCK);
 	ret = mmc_of_parse(dev, &plat->cfg);
-	if (ret)
-		return ret;
 
 	return ret;
 }
@@ -286,8 +475,21 @@ static int spacemit_sdhci_bind(struct udevice *dev)
 	return sdhci_bind(dev, &plat->mmc, &plat->cfg);
 }
 
+const struct sdhci_ops spacemit_sdhci_ops = {
+	.set_control_reg = spacemit_sdhci_set_control_reg,
+#if CONFIG_IS_ENABLED(MMC_HS400_ES_SUPPORT)
+	.set_enhanced_strobe = spacemit_sdhci_hs400_enhanced_strobe,
+#endif
+};
+
+const struct spacemit_sdhci_driver_data spacemit_sdhci_drv_data = {
+	.ops = &spacemit_sdhci_ops,
+};
+
 static const struct udevice_id spacemit_sdhci_ids[] = {
-	{ .compatible = "spacemit,k1-x-sdhci" },
+	{ .compatible = "spacemit,k1-x-sdhci",
+	  .data = (ulong)&spacemit_sdhci_drv_data,
+	},
 	{ }
 };
 
@@ -295,7 +497,7 @@ U_BOOT_DRIVER(spacemit_sdhci_drv) = {
 	.name		= "spacemit_sdhci",
 	.id		= UCLASS_MMC,
 	.of_match	= spacemit_sdhci_ids,
-	.of_to_plat = spacemit_sdhci_ofdata_to_platdata,
+	.of_to_plat = spacemit_sdhci_of_to_plat,
 	.ops		= &sdhci_ops,
 	.bind		= spacemit_sdhci_bind,
 	.probe		= spacemit_sdhci_probe,
