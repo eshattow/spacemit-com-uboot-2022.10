6fc6cb35ab9f9b97adf57a4359e667d1deb08db0
zhouxl <zhouxl@spacemit.com>
2024-02-07 14:15:44 +0800

    remove build-in ethaddr which would read from eeprom or be generated, ethaddr in env has the highest priority
    Change-Id: I671402476214ee113abb81afb330543a13067395

--- include/configs/k1-x.h
+++ include/configs/k1-x.h
@@ -119,7 +119,6 @@ enum board_boot_mode {
 	"scriptaddr=0x2c100000\0" \
 	"pxefile_addr_r=0x2c200000\0" \
 	"ramdisk_addr_r=0x2c300000\0" \
-	"ethaddr=00:00:23:34:45:66\0" \
 	"ipaddr=192.168.1.15\0" \
 	"netmask=255.255.255.0\0" \
 	"serverip=10.0.92.134\0" \
