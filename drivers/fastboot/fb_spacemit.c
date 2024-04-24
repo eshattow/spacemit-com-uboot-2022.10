e39cf9a039dbfa65c18acf290bd7192560be4da0
zhouxl <zhouxl@spacemit.com>
2024-04-24 15:24:19 +0800

    add ddr cs number configuration write support
    Change-Id: I7c09f88c5a1e15b08350c64c2885da12e099d4a1

--- drivers/fastboot/fb_spacemit.c
+++ drivers/fastboot/fb_spacemit.c
@@ -848,6 +848,7 @@ const struct oem_config_info config_info[] = {
 	{ "device_version", TLV_CODE_DEVICE_VERSION, 3, NULL },
 	{ "manufacturer", TLV_CODE_MANUF_NAME, 32, NULL },
 	{ "sdk_version", TLV_CODE_SDK_VERSION, 3, NULL},
+	{ "ddr_cs_num", TLV_CODE_DDR_CSNUM, 3, NULL},
 	{ "pmic_type", TLV_CODE_PMIC_TYPE, 3, NULL},
 	{ "eeprom_i2c_index", TLV_CODE_EEPROM_I2C_INDEX, 3, NULL},
 	{ "eeprom_pin_group", TLV_CODE_EEPROM_PIN_GROUP, 3, NULL},
