eshattow/spacemit-com-uboot-2022.10

commit ffd5e1eddcfbaa39ce44645a89b4e310f7c50152

usb: xhci: Set up endpoints for the first 2 interfaces
The xhci driver currently only does the necessary initialization for
endpoints found in the first interface descriptor. Apple USB keyboards
(released 2021) use the second interface descriptor for the HID keyboard
boot protocol. To allow USB drivers to use endpoints from other
interface descriptors the xhci driver needs to ensure these endpoints
are initialized as well.
Use USB_MAX_ACTIVE_INTERFACES to control how many interface descriptors
are considered during endpoint initialisation.
For now define it to 2 as that is sufficient for supporting the Apple
keyboards.

Reviewed-by: Marek Vasut <marex@denx.de>
Reviewed-by: Neal Gompa <neal@gompa.dev>
Change-Id: I3c517e4bf67798bf8adcf23bca88611d5a0e8a47
Signed-off-by: Janne Grunau <j@jannau.net>

drivers/usb/host/xhci.c
@@ -606,24 +606,28 @@ static int xhci_set_configuration(struct usb_device *udev)
 	int slot_id = udev->slot_id;
 	struct xhci_virt_device *virt_dev = ctrl->devs[slot_id];
 	struct usb_interface *ifdesc;
+	unsigned int ifnum;
+	unsigned int max_ifnum = min((unsigned int)USB_MAX_ACTIVE_INTERFACES,
+				     (unsigned int)udev->config.no_of_if);
 
 	out_ctx = virt_dev->out_ctx;
 	in_ctx = virt_dev->in_ctx;
-
-	num_of_ep = udev->config.if_desc[0].no_of_ep;
-	ifdesc = &udev->config.if_desc[0];
 
 	ctrl_ctx = xhci_get_input_control_ctx(in_ctx);
 	/* Initialize the input context control */
 	ctrl_ctx->add_flags = cpu_to_le32(SLOT_FLAG);
 	ctrl_ctx->drop_flags = 0;
 
-	/* EP_FLAG gives values 1 & 4 for EP1OUT and EP2IN */
-	for (cur_ep = 0; cur_ep < num_of_ep; cur_ep++) {
-		ep_flag = xhci_get_ep_index(&ifdesc->ep_desc[cur_ep]);
-		ctrl_ctx->add_flags |= cpu_to_le32(1 << (ep_flag + 1));
-		if (max_ep_flag < ep_flag)
-			max_ep_flag = ep_flag;
+	for (ifnum = 0; ifnum < max_ifnum; ifnum++) {
+		ifdesc = &udev->config.if_desc[ifnum];
+		num_of_ep = ifdesc->no_of_ep;
+		/* EP_FLAG gives values 1 & 4 for EP1OUT and EP2IN */
+		for (cur_ep = 0; cur_ep < num_of_ep; cur_ep++) {
+			ep_flag = xhci_get_ep_index(&ifdesc->ep_desc[cur_ep]);
+			ctrl_ctx->add_flags |= cpu_to_le32(1 << (ep_flag + 1));
+			if (max_ep_flag < ep_flag)
+				max_ep_flag = ep_flag;
+		}
 	}
 
 	xhci_inval_cache((uintptr_t)out_ctx->bytes, out_ctx->size);
@@ -637,9 +641,12 @@ static int xhci_set_configuration(struct usb_device *udev)
 	xhci_endpoint_copy(ctrl, in_ctx, out_ctx, 0);
 
 	/* filling up ep contexts */
-	err = xhci_init_ep_contexts_if(udev, ctrl, virt_dev, ifdesc);
-	if (err < 0)
-		return err;
+	for (ifnum = 0; ifnum < max_ifnum; ifnum++) {
+		ifdesc = &udev->config.if_desc[ifnum];
+		err = xhci_init_ep_contexts_if(udev, ctrl, virt_dev, ifdesc);
+		if (err < 0)
+			return err;
+	}
 
 	return xhci_configure_endpoints(udev, false);
 }

include/usb.h
@@ -46,6 +46,12 @@
  */
 #define USB_TIMEOUT_MS(pipe) (usb_pipebulk(pipe) ? 5000 : 1000)
 
+/*
+ * The xhcd hcd driver prepares only a limited number interfaces / endpoints.
+ * Define this limit so that drivers do not exceed it.
+ */
+#define USB_MAX_ACTIVE_INTERFACES	2
+
 /* device request (setup) */
 struct devrequest {
 	__u8	requesttype;

commit 373049eab0ed37f942e43d94c4efaafe0774583c

usb: host: xhci: don't init USB_CLASS_VIDEO interfaces
Previous upstream commit ffd5e1eddcf have refactored xhci to
initialize at lesat two interface to make sure Apple USB Keyboard
is usable.

However it's found that some UVC cameras have multiple interfaces
could be influences to cause Errors.

Because we don't support much isoc ep resources to allocate in
current code, the simplest fix is to ban USB_CLASS_VIDEO iface.

Related error messages:

[   4.479] ERROR: Configure Endpoint command returned completion code 17.
Failed to configure xHCI endpoint
[   4.485] failed to set default configuration len 1220, status 80000000
[   4.492] Unexpected XHCI event TRB, skipping... (7dff0a80 00000000 11000000 03008401)

Fixes: ffd5e1eddcf (usb: xhci: Set up endpoints for the first 2 interfaces)
Change-Id: Id37df1eaee06c1248c48cced51169c60b2229167
Signed-off-by: Junzhong Pan <junzhong.pan@spacemit.com>

drivers/usb/host/xhci.c
@@ -620,6 +620,8 @@ static int xhci_set_configuration(struct usb_device *udev)
 
 	for (ifnum = 0; ifnum < max_ifnum; ifnum++) {
 		ifdesc = &udev->config.if_desc[ifnum];
+		if (ifdesc->desc.bInterfaceClass == USB_CLASS_VIDEO)
+			continue;
 		num_of_ep = ifdesc->no_of_ep;
 		/* EP_FLAG gives values 1 & 4 for EP1OUT and EP2IN */
 		for (cur_ep = 0; cur_ep < num_of_ep; cur_ep++) {
@@ -643,6 +645,8 @@ static int xhci_set_configuration(struct usb_device *udev)
 	/* filling up ep contexts */
 	for (ifnum = 0; ifnum < max_ifnum; ifnum++) {
 		ifdesc = &udev->config.if_desc[ifnum];
+		if (ifdesc->desc.bInterfaceClass == USB_CLASS_VIDEO)
+			continue;
 		err = xhci_init_ep_contexts_if(udev, ctrl, virt_dev, ifdesc);
 		if (err < 0)
 			return err;