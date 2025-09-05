7900eba470a3a4be19ecce3d3ee2041f2b746e25
“huangxicen” <xicen.huang@spacemit.com>
2025-09-05 11:39:27 +0800

    gpio: Add support for K3 SoC and unify driver
    This patch extends the spacemit k1x GPIO driver
    to add support for the K3 SoC,
    creating a unified driver for both K1 and K3 platforms.
    
    Change-Id: I5efc7d8b39340f52c4d4d3cbc216838a9202b544

--- drivers/gpio/k1x_gpio.c
+++ drivers/gpio/k1x_gpio.c
@@ -1,13 +1,12 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
- * spacemit k1x gpio driver
+ * spacemit k1x/k3 gpio driver
  *
- * Copyright (C) 2023 Spacemit
+ * Copyright (C) 2025 Spacemit
  *
  */
 
 #include <common.h>
-#include <asm/arch/gpio.h>
 #include <asm/io.h>
 #include <errno.h>
 #include <asm/gpio.h>
@@ -15,7 +14,6 @@
 #include <clk.h>
 #include "k1x_gpio.h"
 
-#ifdef CONFIG_DM_GPIO
 #include <dm/read.h>
 #include <dm/device.h>
 #include <dm/device_compat.h>
@@ -23,9 +21,6 @@
 #include <dm/pinctrl.h>
 
 static void __iomem *k1x_gpio_base;
-#else
-#define K1X_GPIO_BASE   0xd4019000
-#endif
 
 #ifndef K1X_MAX_GPIO
 #define K1X_MAX_GPIO	128
@@ -35,14 +30,72 @@ static void __iomem *k1x_gpio_base;
 #define GPIO_TO_BIT(gp)		(1 << (gp & 0x1f))
 #define GPIO_VAL(gp, val)	((val >> (gp & 0x1f)) & 0x01)
 
+/* K1 register offsets */
+static const struct spacemit_gpio_reg_offsets k1_regs = {
+	.gplr    = 0x0,
+	.gpdr    = 0xc,
+	.gpsr    = 0x18,
+	.gpcr    = 0x24,
+	.grer    = 0x30,
+	.gfer    = 0x3c,
+	.gedr    = 0x48,
+	.gsdr    = 0x54,
+	.gcdr    = 0x60,
+	.gsrer   = 0x6c,
+	.gcrer   = 0x78,
+	.gsfer   = 0x84,
+	.gcfer   = 0x90,
+	.gapmask = 0x9c,
+	.gcpmask = 0xA8,
+};
+
+/* K3 register offsets */
+static const struct spacemit_gpio_reg_offsets k3_regs = {
+	.gplr    = 0x0,
+	.gpdr    = 0x4,
+	.gpsr    = 0x8,
+	.gpcr    = 0xc,
+	.grer    = 0x10,
+	.gfer    = 0x14,
+	.gedr    = 0x18,
+	.gsdr    = 0x1c,
+	.gcdr    = 0x20,
+	.gsrer   = 0x24,
+	.gcrer   = 0x28,
+	.gsfer   = 0x2c,
+	.gcfer   = 0x30,
+	.gapmask = 0x34,
+	.gcpmask = 0x38,
+};
+
+/* K1 bank offsets */
+static const unsigned long k1_bank_offsets[] = {0x0, 0x4, 0x8, 0x100};
+
+/* K3 bank offsets */
+static const unsigned long k3_bank_offsets[] = {0x0, 0x40, 0x80, 0x100};
+
+static const struct spacemit_gpio_chip_data k1_chip_data = {
+	.regs = &k1_regs,
+	.bank_offsets = k1_bank_offsets,
+	.num_banks = ARRAY_SIZE(k1_bank_offsets),
+	.max_gpio = 128,
+};
+
+static const struct spacemit_gpio_chip_data k3_chip_data = {
+	.regs = &k3_regs,
+	.bank_offsets = k3_bank_offsets,
+	.num_banks = ARRAY_SIZE(k3_bank_offsets),
+	.max_gpio = 128,
+};
+
 /**
- * struct k1x_gpio_pctrl_map - gpio and pinctrl mapping
+ * struct spacemit_gpio_pctrl_map - gpio and pinctrl mapping
  * @gpio_pin:	start of gpio number in gpio-ranges
  * @pctrl_pin:	start of pinctrl number in gpio-ranges
  * @npins:	total number of pins in gpio-ranges
  * @node:	list node
  */
-struct k1x_gpio_pctrl_map {
+struct spacemit_gpio_pctrl_map {
 	u32 gpio_pin;
 	u32 pctrl_pin;
 	u32 npins;
@@ -50,108 +103,125 @@ struct k1x_gpio_pctrl_map {
 };
 
 /**
- * struct k1x_gpio_pctrl_map - gpio device instance
+ * struct spacemit_gpio_plat - gpio device instance
  * @pinctrl_dev:pointer to gpio device
  * @gpiomap:	list node having mapping between gpio and pinctrl
  * @base:	I/O register base address of gpio device
+ * @chip_data:	chip-specific data (K1 or K3)
  * @name:	gpio device name, ex GPIO0, GPIO1
  * @ngpios:	total number of gpios
  */
-struct k1x_gpio_plat {
+struct spacemit_gpio_plat {
 	struct udevice *pinctrl_dev;
 	struct list_head gpiomap;
 	void __iomem *base;
+	const struct spacemit_gpio_chip_data *chip_data;
 };
 
-static inline void *get_gpio_base(int bank)
+static inline void *get_gpio_base(int bank, const struct spacemit_gpio_chip_data *chip_data)
 {
-	const unsigned long offset[] = {0, 4, 8, 0x100};
-	/* gpio register bank offset */
-#ifdef CONFIG_DM_GPIO
-	return (struct gpio_reg *)(k1x_gpio_base + offset[bank]);
-#else
-	return (struct gpio_reg *)(K1X_GPIO_BASE + offset[bank]);
-#endif
+	if (bank >= chip_data->num_banks) {
+		printf("%s: Invalid bank %d\n", __func__, bank);
+		return NULL;
+	}
+	return (void *)(k1x_gpio_base + chip_data->bank_offsets[bank]);
 }
 
-static int _gpio_direction_input(unsigned gpio)
+static int spacemit_gpio_direction_input(unsigned gpio, const struct spacemit_gpio_chip_data *chip_data)
 {
-	struct gpio_reg *gpio_reg_bank;
+	void __iomem *gpio_base;
+	void __iomem *reg_addr;
 
-	if (gpio >= K1X_MAX_GPIO) {
+	if (gpio >= chip_data->max_gpio) {
 		printf("%s: Invalid GPIO %d\n", __func__, gpio);
 		return -1;
 	}
 
-	gpio_reg_bank = get_gpio_base(GPIO_TO_REG(gpio));
-	writel(GPIO_TO_BIT(gpio), &gpio_reg_bank->gcdr);
+	gpio_base = get_gpio_base(GPIO_TO_REG(gpio), chip_data);
+	if (!gpio_base)
+		return -1;
+	
+	reg_addr = gpio_base + chip_data->regs->gcdr;
+	writel(GPIO_TO_BIT(gpio), reg_addr);
 	return 0;
 }
 
-static int _gpio_set_value(unsigned gpio, int value)
+static int spacemit_gpio_set_value(unsigned gpio, int value, const struct spacemit_gpio_chip_data *chip_data)
 {
-	struct gpio_reg *gpio_reg_bank;
+	void __iomem *gpio_base;
+	void __iomem *reg_addr;
 
-	if (gpio >= K1X_MAX_GPIO) {
+	if (gpio >= chip_data->max_gpio) {
 		printf("%s: Invalid GPIO %d\n", __func__, gpio);
 		return -1;
 	}
 
-	gpio_reg_bank = get_gpio_base(GPIO_TO_REG(gpio));
-	if (value)
-		writel(GPIO_TO_BIT(gpio), &gpio_reg_bank->gpsr);
-	else
-		writel(GPIO_TO_BIT(gpio), &gpio_reg_bank->gpcr);
+	gpio_base = get_gpio_base(GPIO_TO_REG(gpio), chip_data);
+	if (!gpio_base)
+		return -1;
 
+	if (value) {
+		reg_addr = gpio_base + chip_data->regs->gpsr;
+	} else {
+		reg_addr = gpio_base + chip_data->regs->gpcr;
+	}
+
+	writel(GPIO_TO_BIT(gpio), reg_addr);
 	return 0;
 }
 
-static int _gpio_direction_output(unsigned gpio, int value)
+static int spacemit_gpio_direction_output(unsigned gpio, int value, const struct spacemit_gpio_chip_data *chip_data)
 {
-	struct gpio_reg *gpio_reg_bank;
+	void __iomem *gpio_base;
+	void __iomem *reg_addr;
 
-	if (gpio >= K1X_MAX_GPIO) {
+	if (gpio >= chip_data->max_gpio) {
 		printf("%s: Invalid GPIO %d\n", __func__, gpio);
 		return -1;
 	}
 
-	gpio_reg_bank = get_gpio_base(GPIO_TO_REG(gpio));
-	writel(GPIO_TO_BIT(gpio), &gpio_reg_bank->gsdr);
-	_gpio_set_value(gpio, value);
+	gpio_base = get_gpio_base(GPIO_TO_REG(gpio), chip_data);
+	if (!gpio_base)
+		return -1;
+	
+	reg_addr = gpio_base + chip_data->regs->gsdr;
+	writel(GPIO_TO_BIT(gpio), reg_addr);
+	spacemit_gpio_set_value(gpio, value, chip_data);
 	return 0;
 }
 
-static int _gpio_get_value(unsigned gpio)
+static int spacemit_gpio_get_value(unsigned gpio, const struct spacemit_gpio_chip_data *chip_data)
 {
-	struct gpio_reg *gpio_reg_bank;
+	void __iomem *gpio_base;
+	void __iomem *reg_addr;
 	u32 gpio_val;
 
-	if (gpio >= K1X_MAX_GPIO) {
+	if (gpio >= chip_data->max_gpio) {
 		printf("%s: Invalid GPIO %d\n", __func__, gpio);
 		return -1;
 	}
 
-	gpio_reg_bank = get_gpio_base(GPIO_TO_REG(gpio));
-	gpio_val = readl(&gpio_reg_bank->gplr);
+	gpio_base = get_gpio_base(GPIO_TO_REG(gpio), chip_data);
+	if (!gpio_base)
+		return -1;
+
+	reg_addr = gpio_base + chip_data->regs->gplr;
+	gpio_val = readl(reg_addr);
 
 	return GPIO_VAL(gpio, gpio_val);
 }
 
-#ifdef CONFIG_DM_GPIO
 /**
- * k1x_get_gpio_pctrl_mapping() - get associated pinctrl pin from gpio pin
- *
- * @plat: k1x GPIO device
- * @gpio: GPIO pin
+ * spacemit_get_pctrl_from_gpio() - get associated pinctrl pin from gpio pin
  */
-static int k1x_get_pctrl_from_gpio(struct k1x_gpio_plat *plat, u32 gpio, u32 *pctrl_pin)
+static int spacemit_get_pctrl_from_gpio(struct spacemit_gpio_plat *plat, u32 gpio, u32 *pctrl_pin)
 {
-	struct k1x_gpio_pctrl_map *range = NULL;
+	struct spacemit_gpio_pctrl_map *range = NULL;
 	struct list_head *pos, *tmp;
 	int ret = -EINVAL;
 
 	list_for_each_safe(pos, tmp, &plat->gpiomap) {
-		range = list_entry(pos, struct k1x_gpio_pctrl_map, node);
+		range = list_entry(pos, struct spacemit_gpio_pctrl_map, node);
 		if (gpio >= range->gpio_pin &&
 		    gpio < (range->gpio_pin + range->npins)) {
 			*pctrl_pin = range->pctrl_pin + (gpio - range->gpio_pin);
@@ -164,18 +234,12 @@ static int k1x_get_pctrl_from_gpio(struct k1x_gpio_plat *plat, u32 gpio, u32 *pc
 }
 
 /**
- * k1x_get_gpio_pctrl_mapping() - get mapping between gpio and pinctrl
- *
- * Read dt node "gpio-ranges" to get gpio and pinctrl mapping and store
- * in private data structure to use it later while enabling gpio.
- *
- * @dev: pointer to GPIO device
- * Return: 0 on success and -ENOMEM on failure
+ * spacemit_get_gpio_pctrl_mapping() - get mapping between gpio and pinctrl
  */
-static int k1x_get_gpio_pctrl_mapping(struct udevice *dev)
+static int spacemit_get_gpio_pctrl_mapping(struct udevice *dev)
 {
-	struct k1x_gpio_plat *plat = dev_get_plat(dev);
-	struct k1x_gpio_pctrl_map *range = NULL;
+	struct spacemit_gpio_plat *plat = dev_get_plat(dev);
+	struct spacemit_gpio_pctrl_map *range = NULL;
 	struct ofnode_phandle_args args;
 	int index = 0, ret;
 
@@ -185,7 +249,7 @@ static int k1x_get_gpio_pctrl_mapping(struct udevice *dev)
 		if (ret)
 			break;
 
-		range = (struct k1x_gpio_pctrl_map *)devm_kzalloc(dev, sizeof(*range), GFP_KERNEL);
+		range = (struct spacemit_gpio_pctrl_map *)devm_kzalloc(dev, sizeof(*range), GFP_KERNEL);
 		if (!range)
 			return -ENOMEM;
 
@@ -198,12 +262,19 @@ static int k1x_get_gpio_pctrl_mapping(struct udevice *dev)
 	return 0;
 }
 
-static int gpio_k1x_probe(struct udevice *dev)
+static int spacemit_gpio_probe(struct udevice *dev)
 {
-	struct k1x_gpio_plat *plat = dev_get_plat(dev);
+	struct spacemit_gpio_plat *plat = dev_get_plat(dev);
 	struct gpio_dev_priv *uc_priv = dev_get_uclass_priv(dev);
 	struct clk gpio_clk;
 	int ret = 0;
+
+	/* Get chip-specific data from device tree */
+	plat->chip_data = (const struct spacemit_gpio_chip_data *)dev_get_driver_data(dev);
+	if (!plat->chip_data) {
+		debug("%s: Failed to get chip data\n", __func__);
+		return -EINVAL;
+	}
 
 	plat->base = dev_remap_addr_index(dev, 0);
 	if (!plat->base) {
@@ -217,7 +288,7 @@ static int gpio_k1x_probe(struct udevice *dev)
 				     &plat->pinctrl_dev);
 	if (ret == 0) {
 		INIT_LIST_HEAD(&plat->gpiomap);
-		ret = k1x_get_gpio_pctrl_mapping(dev);
+		ret = spacemit_get_gpio_pctrl_mapping(dev);
 		if (ret < 0) {
 			dev_err(dev, "%s: Failed to get gpio to pctrl map ret(%d)\n",
 				__func__, ret);
@@ -228,22 +299,23 @@ static int gpio_k1x_probe(struct udevice *dev)
 	}
 
 	ret = clk_get_by_index(dev, 0, &gpio_clk);
-	if (ret) 
+	if (ret)
 		return ret;
 	clk_enable(&gpio_clk);
 
 	return 0;
 }
 
-static const struct udevice_id gpio_k1x_ids[] = {
-	{ .compatible = "spacemit,k1x-gpio" },
+static const struct udevice_id spacemit_gpio_ids[] = {
+	{ .compatible = "spacemit,k1x-gpio", .data = (ulong)&k1_chip_data },
+	{ .compatible = "spacemit,k3-gpio", .data = (ulong)&k3_chip_data },
 	{ }
 };
 
-static int k1x_gpio_request(struct udevice *dev, unsigned gpio,
+static int spacemit_gpio_request(struct udevice *dev, unsigned gpio,
 			     const char *label)
 {
-	struct k1x_gpio_plat *plat = dev_get_plat(dev);
+	struct spacemit_gpio_plat *plat = dev_get_plat(dev);
 	u32 pctrl;
 	int ret = 0;
 
@@ -251,84 +323,51 @@ static int k1x_gpio_request(struct udevice *dev, unsigned gpio,
 	if (!plat->pinctrl_dev)
 		return 0;
 
-	ret = k1x_get_pctrl_from_gpio(plat, gpio, &pctrl);
+	ret = spacemit_get_pctrl_from_gpio(plat, gpio, &pctrl);
 	if (ret < 0)
 		return 0;
 
 	return pinctrl_request(plat->pinctrl_dev, pctrl, 0);
 }
 
-static int k1x_gpio_get_value(struct udevice *dev, unsigned int gpio)
+static int spacemit_gpio_get_value_dm(struct udevice *dev, unsigned int gpio)
 {
-	return _gpio_get_value(gpio);
+	struct spacemit_gpio_plat *plat = dev_get_plat(dev);
+	return spacemit_gpio_get_value(gpio, plat->chip_data);
 }
 
-static int k1x_gpio_set_value(struct udevice *dev, unsigned int gpio,
+static int spacemit_gpio_set_value_dm(struct udevice *dev, unsigned int gpio,
 				   int value)
 {
-	return _gpio_set_value(gpio, value);
+	struct spacemit_gpio_plat *plat = dev_get_plat(dev);
+	return spacemit_gpio_set_value(gpio, value, plat->chip_data);
 }
 
-static int k1x_gpio_direction_input(struct udevice *dev, unsigned int gpio)
+static int spacemit_gpio_direction_input_dm(struct udevice *dev, unsigned int gpio)
 {
-	return _gpio_direction_input(gpio);
+	struct spacemit_gpio_plat *plat = dev_get_plat(dev);
+	return spacemit_gpio_direction_input(gpio, plat->chip_data);
 }
 
-static int k1x_gpio_direction_output(struct udevice *dev, unsigned int gpio,
+static int spacemit_gpio_direction_output_dm(struct udevice *dev, unsigned int gpio,
 					  int value)
 {
-	return _gpio_direction_output(gpio, value);
+	struct spacemit_gpio_plat *plat = dev_get_plat(dev);
+	return spacemit_gpio_direction_output(gpio, value, plat->chip_data);
 }
 
-static const struct dm_gpio_ops gpio_k1x_ops = {
-	.request		= k1x_gpio_request,
-	.direction_input	= k1x_gpio_direction_input,
-	.direction_output	= k1x_gpio_direction_output,
-	.get_value		= k1x_gpio_get_value,
-	.set_value		= k1x_gpio_set_value,
+static const struct dm_gpio_ops spacemit_gpio_ops = {
+	.request		= spacemit_gpio_request,
+	.direction_input	= spacemit_gpio_direction_input_dm,
+	.direction_output	= spacemit_gpio_direction_output_dm,
+	.get_value		= spacemit_gpio_get_value_dm,
+	.set_value		= spacemit_gpio_set_value_dm,
 };
-U_BOOT_DRIVER(gpio_k1x) = {
-	.name	= "gpio_k1x",
+U_BOOT_DRIVER(gpio_spacemit) = {
+	.name	= "gpio_spacemit",
 	.id	= UCLASS_GPIO,
-	.ops	= &gpio_k1x_ops,
-	.of_match = gpio_k1x_ids,
-	.probe	= gpio_k1x_probe,
-	.plat_auto	= sizeof(struct k1x_gpio_plat),
+	.ops	= &spacemit_gpio_ops,
+	.of_match = spacemit_gpio_ids,
+	.probe	= spacemit_gpio_probe,
+	.plat_auto	= sizeof(struct spacemit_gpio_plat),
 };
-
-#else
-
-int gpio_request(unsigned gpio, const char *label)
-{
-	if (gpio >= K1X_MAX_GPIO) {
-		printf("%s: Invalid GPIO requested %d\n", __func__, gpio);
-		return -1;
-	}
-	return 0;
-}
-
-int gpio_free(unsigned gpio)
-{
-	return 0;
-}
-
-int gpio_direction_input(unsigned gpio)
-{
-	return _gpio_direction_input(gpio);
-}
-
-int gpio_direction_output(unsigned gpio, int value)
-{
-	return _gpio_direction_output(gpio, value);
-}
-
-int gpio_get_value(unsigned gpio)
-{
-	return _gpio_get_value(gpio);
-}
-
-int gpio_set_value(unsigned gpio, int value)
-{
-	return _gpio_set_value(gpio, value);
-}
-#endif
--- drivers/gpio/k1x_gpio.h
+++ drivers/gpio/k1x_gpio.h
@@ -4,40 +4,34 @@
 #include <common.h>
 
 /*
- * GPIO Register map for k1x
+ * GPIO Register offsets structure for different platforms
  */
-struct gpio_reg {
-	u32 gplr;	/* Pin Level Register - 0x0000 */
-	u32 pad0[2];
-	u32 gpdr;	/* Pin Direction Register - 0x000C */
-	u32 pad1[2];
-	u32 gpsr;	/* Pin Output Set Register - 0x0018 */
-	u32 pad2[2];
-	u32 gpcr;	/* Pin Output Clear Register - 0x0024 */
-	u32 pad3[2];
-	u32 grer;	/* Rising-Edge Detect Enable Register - 0x0030 */
-	u32 pad4[2];
-	u32 gfer;	/* Falling-Edge Detect Enable Register - 0x003C */
-	u32 pad5[2];
-	u32 gedr;	/* Edge Detect Status Register - 0x0048 */
-	u32 pad6[2];
-	u32 gsdr;	/* Bitwise Set of GPIO Direction Register - 0x0054 */
-	u32 pad7[2];
-	u32 gcdr;	/* Bitwise Clear of GPIO Direction Register - 0x0060 */
-	u32 pad8[2];
-	u32 gsrer;	/* Bitwise Set of Rising-Edge Detect Enable
-			   Register - 0x006C */
-	u32 pad9[2];
-	u32 gcrer;	/* Bitwise Clear of Rising-Edge Detect Enable
-			   Register - 0x0078 */
-	u32 pad10[2];
-	u32 gsfer;	/* Bitwise Set of Falling-Edge Detect Enable
-			   Register - 0x0084 */
-	u32 pad11[2];
-	u32 gcfer;	/* Bitwise Clear of Falling-Edge Detect Enable
-			   Register - 0x0090 */
-	u32 pad12[2];
-	u32 apmask;	/* Bitwise Mask of Edge Detect Register - 0x009C */
+struct spacemit_gpio_reg_offsets {
+	u32 gplr;	/* Pin Level Register */
+	u32 gpdr;	/* Pin Direction Register */
+	u32 gpsr;	/* Pin Output Set Register */
+	u32 gpcr;	/* Pin Output Clear Register */
+	u32 grer;	/* Rising-Edge Detect Enable Register */
+	u32 gfer;	/* Falling-Edge Detect Enable Register */
+	u32 gedr;	/* Edge Detect Status Register */
+	u32 gsdr;	/* Bitwise Set of GPIO Direction Register */
+	u32 gcdr;	/* Bitwise Clear of GPIO Direction Register */
+	u32 gsrer;	/* Bitwise Set of Rising-Edge Detect Enable Register */
+	u32 gcrer;	/* Bitwise Clear of Rising-Edge Detect Enable Register */
+	u32 gsfer;	/* Bitwise Set of Falling-Edge Detect Enable Register */
+	u32 gcfer;	/* Bitwise Clear of Falling-Edge Detect Enable Register */
+	u32 gapmask;	/* Bitwise Mask of Edge Detect Register */
+	u32 gcpmask;	/* Bitwise Clear Mask of Edge Detect Register */
 };
+
+/* Chip-specific data structure */
+struct spacemit_gpio_chip_data {
+	const struct spacemit_gpio_reg_offsets *regs;
+	const unsigned long *bank_offsets;
+	unsigned int num_banks;
+	unsigned int max_gpio;
+};
+
+
 
 #endif /* __K1X_GPIO_H__ */
