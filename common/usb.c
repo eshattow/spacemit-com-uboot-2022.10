u-boot/u-boot

commit 687ab54560809e01a10652ef82dbac6ae72deb91

usb: introduce a separate config option for DM USB device
Using CONFIG_DM_USB for this purpose prevents using DM_USB for host and not
for device.

Signed-off-by: Jean-Jacques Hiblot <jjhiblot@ti.com>
Reviewed-by: Lukasz Majewski <lukma@denx.de>

drivers/usb/Kconfig
@@ -52,6 +52,20 @@ config SPL_DM_USB
 	depends on DM_USB
 	default y
 
+config DM_USB_GADGET
+	bool "Enable driver model for USB Gadget"
+	depends on DM_USB
+	help
+	  Enable driver model for USB Gadget (Peripheral
+	  mode)
+
+config SPL_DM_USB_GADGET
+	bool "Enable driver model for USB Gadget in sPL"
+	depends on SPL_DM_USB
+	help
+	  Enable driver model for USB Gadget in SPL
+	  (Peripheral mode)
+
 source "drivers/usb/host/Kconfig"
 
 source "drivers/usb/dwc3/Kconfig"

drivers/usb/dwc3/core.c
@@ -789,8 +789,7 @@ MODULE_AUTHOR("Felipe Balbi <balbi@ti.com>");
 MODULE_LICENSE("GPL v2");
 MODULE_DESCRIPTION("DesignWare USB3 DRD Controller Driver");
 
-#if CONFIG_IS_ENABLED(DM_USB)
-
+#if CONFIG_IS_ENABLED(DM_USB_GADGET)
 int dwc3_init(struct dwc3 *dwc)
 {
 	int ret;

arch/arm/Kconfig
@@ -932,6 +932,7 @@ config ARCH_ZYNQMP_R5
 	select DM_SERIAL
 	select OF_CONTROL
 	imply CMD_DM
+	imply DM_USB_GADGET
 
 config ARCH_ZYNQMP
 	bool "Xilinx ZynqMP based platform"
@@ -949,6 +950,7 @@ config ARCH_ZYNQMP
 	imply CMD_DM
 	imply FAT_WRITE
 	imply MP
+	imply DM_USB_GADGET
 
 config TEGRA
 	bool "NVIDIA Tegra"

drivers/usb/dwc3/dwc3-generic.c
@@ -21,8 +21,7 @@
 #include "gadget.h"
 #include "linux-compat.h"
 
-DECLARE_GLOBAL_DATA_PTR;
-
+#if CONFIG_IS_ENABLED(DM_USB_GADGET)
 int usb_gadget_handle_interrupts(int index)
 {
 	struct dwc3 *priv;
@@ -97,6 +96,7 @@ U_BOOT_DRIVER(dwc3_generic_peripheral) = {
 	.priv_auto_alloc_size = sizeof(struct dwc3),
 	.flags	= DM_FLAG_ALLOC_PRIV_DMA,
 };
+#endif
 
 static int dwc3_generic_bind(struct udevice *parent)
 {

eshattow/spacemit-com-uboot-2022.10

commit 878d02d2b487f051091ab22a792a84bc29fd5d91

usb: separate DM_USB and DM_USB_GADGET completely
Current DM_USB_GADGET relies on DM_USB, forcing DM_USB to be enabled
when DM_USB_GADGET is enabled. This dependency is unnecessary, fix it.

Change-Id: Ib875e28e298f5b949ccb7f45f642d27185ac110f
Fixes: 687ab5456080 ("usb: introduce a separate config option for DM USB device")
Signed-off-by: Junzhong Pan <junzhong.pan@spacemit.com>

common/usb.c
@@ -45,7 +45,7 @@
 static int asynch_allowed;
 char usb_started; /* flag for the started/stopped USB status */
 
-#if !CONFIG_IS_ENABLED(DM_USB)
+#if CONFIG_IS_ENABLED(USB_HOST) && !CONFIG_IS_ENABLED(DM_USB)
 static struct usb_device usb_dev[USB_MAX_DEVICE];
 static int dev_index;
 
@@ -859,7 +859,7 @@ int usb_string(struct usb_device *dev, int index, char *buf, size_t size)
  * the USB device are static allocated [USB_MAX_DEVICE].
  */
 
-#if !CONFIG_IS_ENABLED(DM_USB)
+#if CONFIG_IS_ENABLED(USB_HOST) && !CONFIG_IS_ENABLED(DM_USB)
 
 /* returns a pointer to the device with the index [index].
  * if the device is not assigned (dev->devnum==-1) returns NULL

drivers/usb/Kconfig
@@ -55,14 +55,14 @@ config SPL_DM_USB
 
 config DM_USB_GADGET
 	bool "Enable driver model for USB Gadget"
-	depends on DM_USB
+	depends on DM
 	help
 	  Enable driver model for USB Gadget (Peripheral
 	  mode)
 
 config SPL_DM_USB_GADGET
 	bool "Enable driver model for USB Gadget in SPL"
-	depends on SPL_DM_USB
+	depends on SPL_DM
 	help
 	  Enable driver model for USB Gadget in SPL
 	  (Peripheral mode)

drivers/usb/common/Makefile
@@ -4,6 +4,7 @@
 #
 
 obj-$(CONFIG_$(SPL_)DM_USB) += common.o
+obj-$(CONFIG_$(SPL_)DM_USB_GADGET) += common.o
 obj-$(CONFIG_USB_ISP1760) += usb_urb.o
 obj-$(CONFIG_USB_MUSB_HOST) += usb_urb.o
 obj-$(CONFIG_USB_MUSB_GADGET) += usb_urb.o

drivers/usb/dwc3/Kconfig
@@ -32,7 +32,7 @@ config USB_DWC3_GENERIC
 
 config SPL_USB_DWC3_GENERIC
 	bool "Generic implementation of a DWC3 wrapper (aka dwc3 glue) for the SPL"
-	depends on SPL_DM_USB && USB_DWC3 && SPL_MISC
+	depends on (SPL_DM_USB || SPL_DM_USB_GADGET) && USB_DWC3 && SPL_MISC
 	help
 	  Select this for Xilinx ZynqMP and similar Platforms.
 	  This wrapper supports Host and Peripheral operation modes.

drivers/usb/dwc3/core.c
@@ -961,7 +961,7 @@ MODULE_AUTHOR("Felipe Balbi <balbi@ti.com>");
 MODULE_LICENSE("GPL v2");
 MODULE_DESCRIPTION("DesignWare USB3 DRD Controller Driver");
 
-#if CONFIG_IS_ENABLED(PHY) && CONFIG_IS_ENABLED(DM_USB)
+#if CONFIG_IS_ENABLED(PHY) && (CONFIG_IS_ENABLED(DM_USB) || CONFIG_IS_ENABLED(DM_USB_GADGET))
 int dwc3_setup_phy(struct udevice *dev, struct phy_bulk *phys)
 {
 	int ret;
@@ -991,7 +991,7 @@ int dwc3_shutdown_phy(struct udevice *dev, struct phy_bulk *phys)
 }
 #endif
 
-#if CONFIG_IS_ENABLED(DM_USB)
+#if CONFIG_IS_ENABLED(DM_USB) || CONFIG_IS_ENABLED(DM_USB_GADGET)
 void dwc3_of_parse(struct dwc3 *dwc)
 {
 	const u8 *tmp;

drivers/usb/dwc3/core.h
@@ -757,7 +757,7 @@ struct dwc3 {
 	/* device lock */
 	spinlock_t		lock;
 
-#if defined(__UBOOT__) && CONFIG_IS_ENABLED(DM_USB)
+#if defined(__UBOOT__) && (CONFIG_IS_ENABLED(DM_USB) || CONFIG_IS_ENABLED(DM_USB_GADGET))
 	struct udevice		*dev;
 #else
 	struct device		*dev;