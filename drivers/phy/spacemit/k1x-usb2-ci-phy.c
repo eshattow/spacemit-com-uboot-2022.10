6f480a027606ff440efba77e46eb8a0ac26206ef
Junzhong Pan <junzhong.pan@spacemit.com>
2025-12-21 00:39:59 +0800

    k1x-usb2-ci-phy: k3: add puphy8 setting
    TODO: this code is pcie-usb-phy related,  we will decide where to put
    the codes later. Before that, to make spl/uboot both works, just put it
    in the usb2 phy codes.
    
    Change-Id: I35dfc8be587c2899f87fbb9d363bda201c7e83de

--- drivers/phy/spacemit/k1x-usb2-ci-phy.c
+++ drivers/phy/spacemit/k1x-usb2-ci-phy.c
@@ -37,6 +37,15 @@
 
 #define USB2D_CTRL_RESET_TIME_MS	50
 
+#ifdef CONFIG_TARGET_SPACEMIT_K3
+// TODO: this codes is pcie/usb3.0 phy related, but
+// 	 spl won't load the usb3phy driver for now.
+#define PUPHY8_REG_BASE 		0xcad30000
+#define PCIE_PHY_PU_SEL 		(PUPHY8_REG_BASE + 0x40)
+#define PUPHY_OVRD_STATUS 		(1 << 10)
+#define PUPHY_CFG_PHY_STATUS 		(1 << 9)
+#endif
+
 struct mv_usb_phy_priv {
 	void __iomem		*phy_base;
 };
@@ -47,10 +56,15 @@ static int mv_usb_phy_init(struct phy *phy)
 	void __iomem *base = priv->phy_base;
 	uint32_t loops, temp;
 
-	// make sure the usb controller is not under reset process before any configuration
-	udelay(50);
-	writel(0xbec4, base + USB2_PHY_REG26); //24M ref clk
-	udelay(150);
+#ifdef CONFIG_TARGET_SPACEMIT_K3
+	// TODO: this codes is pcie/usb3.0 phy related, but
+	// 	 spl won't load the usb3phy driver for now.
+	temp = readl((void __iomem *)PCIE_PHY_PU_SEL);
+	temp &= ~(PUPHY_CFG_PHY_STATUS);
+	temp |= PUPHY_OVRD_STATUS;
+	writel(temp, (void __iomem *)PCIE_PHY_PU_SEL);
+	udelay(200);
+#endif
 
 	loops = USB2D_CTRL_RESET_TIME_MS * 1000;
 
