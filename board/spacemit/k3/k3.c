5e7cc5b3346fa355af84e94c4b505b1bdf64b4cf
goumin <min.gou@spacemit.com>
2026-01-05 20:03:16 +0800

    Add retimer init.
    Change-Id: If96cc8279c4582bf8a099ce36cf95236e14e602b

--- board/spacemit/k3/k3.c
+++ board/spacemit/k3/k3.c
@@ -342,7 +342,7 @@ void try_flash_image_from_card(void)
 
 int board_late_init(void)
 {
-#if CONFIG_IS_ENABLED(HWMON_SENSORS_CTF2301)
+#if CONFIG_IS_ENABLED(HWMON_SENSORS_CTF2301) || CONFIG_IS_ENABLED(RT7451_RETIMER)
 	struct udevice *dev;
 #endif
 	ulong kernel_start;
@@ -354,6 +354,11 @@ int board_late_init(void)
 	if (ret || !spacemit_espi_is_ready()) {
 		printf("eSPI not ready, disabling kernel EC driver\n");
 		env_set("espi_disabled", "1");
+	} else {
+		/* Probe CrosEC and its children (I2C tunnel devices) */
+		ret = uclass_probe_all(UCLASS_CROS_EC);
+		if (ret)
+			printf("Warn: Failed to probe CrosEC (err=%d)\n", ret);
 	}
 #endif
 
@@ -362,6 +367,13 @@ int board_late_init(void)
 					  DM_DRIVER_GET(ctf2301), &dev);
 	if (ret)
 		printf("Warn: Failed to force init ctf2301 fan (err=%d)\n", ret);
+#endif
+
+#if CONFIG_IS_ENABLED(RT7451_RETIMER)
+	ret = uclass_get_device_by_driver(UCLASS_MISC,
+					  DM_DRIVER_GET(rt7451), &dev);
+	if (ret)
+		printf("Warn: Failed to init RT7451 retimer (err=%d)\n", ret);
 #endif
 
 	set_env_ethaddr();
