eshattow/spacemit-com-uboot-2022.10

commit f0ab22a9401ebffa73402f3ec39109c0b84c9575

usb: dwc3: invalidate dcache on buffer used in interrupt handling
On Qualcomm systems, the setup buffer and even buffers are in
a bad state at interrupt handling, so invalidate the dcache lines
for the setup_buf and event buffer to make sure we read correct
data written by the hardware.

This fixes the following error:
dwc3-generic-peripheral usb@a600000: UNKNOWN IRQ type -1
dwc3-generic-peripheral usb@a600000: UNKNOWN IRQ type 4673109

and invalid situation in dwc3_gadget_giveback() because setup_buf content
is read at 0s and leads to fatal crash fixed by [1].

[1] https://lore.kernel.org/all/20240528-topic-sm8x50-dwc3-gadget-crash-fix-v1-1-58434ab4b3d3@linaro.org/

Reviewed-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>
Change-Id: Ic22ede1a072aa0b793cbf377926d54788eebb85a
Signed-off-by: Neil Armstrong <neil.armstrong@linaro.org>
Reviewed-by: Marek Vasut <marex@denx.de>
Link: https://lore.kernel.org/r/20241011-u-boot-dwc3-gadget-dcache-fixup-v4-3-5f3498d8035b@linaro.org
Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

drivers/usb/dwc3/gadget.c
@@ -2461,6 +2461,8 @@ static irqreturn_t dwc3_process_event_buf(struct dwc3 *dwc, u32 buf)
 	while (left > 0) {
 		union dwc3_event event;
 
+		dwc3_invalidate_cache((uintptr_t)evt->buf, evt->length);
+
 		event.raw = *(u32 *) (evt->buf + evt->lpos);
 
 		dwc3_process_event_entry(dwc, &event);

drivers/usb/dwc3/ep0.c
@@ -743,6 +743,8 @@ static void dwc3_ep0_inspect_setup(struct dwc3 *dwc,
 	if (!dwc->gadget_driver)
 		goto out;
 
+	dwc3_invalidate_cache((uintptr_t)ctrl, sizeof(*ctrl));
+
 	len = le16_to_cpu(ctrl->wLength);
 	if (!len) {
 		dwc->three_stage_setup = false;

drivers/usb/dwc3/io.h
@@ -52,4 +52,12 @@ static inline void dwc3_flush_cache(uintptr_t addr, int length)
 {
 	flush_dcache_range(addr, addr + ROUND(length, CACHELINE_SIZE));
 }
+
+static inline void dwc3_invalidate_cache(uintptr_t addr, int length)
+{
+	uintptr_t start_addr = (uintptr_t)addr & ~(CACHELINE_SIZE - 1);
+	uintptr_t end_addr = ALIGN((uintptr_t)addr + length, CACHELINE_SIZE);
+
+	invalidate_dcache_range((unsigned long)start_addr, (unsigned long)end_addr);
+}
 #endif /* __DRIVERS_USB_DWC3_IO_H */

commit 02885cee9bdb454d2a5a88ebb1d0a41d75385920

usb: dwc3: gadget: don't flush cache after events processed
Origin flush cache logic is broken, from databook 3.00a 6.3.56,
even when EVNTINTRPTMASK is set, the events are queued.

When we are processing event buf, we will read evt->count of event
into cache, but it will be roundup to cacheline size, thus we may read
empty event into cache, but those memory will be updated during our
processing of current round.

If we flush cache back after dwc3_thread_interrupt, we may end up
commiting stale data to the memory and **overriding incoming events**.

After commit f0ab22a940 introduced dwc3_invalidate_cache() and called
in dwc3_process_event_buf(), we should not do anything after
dwc3_thread_interrupt(). We will never update event thus never need to
write it back to the controller.

Fixes: 889239d6b5c1 ("usb: dwc3: gadget: make cache-maintenance on event buffers more robust")
Fixes: f0ab22a9401e ("usb: dwc3: invalidate dcache on buffer used in interrupt handling")
Change-Id: Ied70fd2a2b886692dc73e70bb481245958939ea2
Signed-off-by: Junzhong Pan <junzhong.pan@spacemit.com>

drivers/usb/dwc3/gadget.c
@@ -2680,15 +2680,7 @@ void dwc3_gadget_uboot_handle_interrupt(struct dwc3 *dwc)
 	int ret = dwc3_interrupt(0, dwc);
 
 	if (ret == IRQ_WAKE_THREAD) {
-		int i;
-		struct dwc3_event_buffer *evt;
-
 		dwc3_thread_interrupt(0, dwc);
-
-		/* Clean + Invalidate the buffers after touching them */
-		for (i = 0; i < dwc->num_event_buffers; i++) {
-			evt = dwc->ev_buffs[i];
-			dwc3_flush_cache((uintptr_t)evt->buf, evt->length);
-		}
+		/* Buffers ared invalidated in dwc3_process_event_buf() */
 	}
 }