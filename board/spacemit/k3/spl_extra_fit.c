60485d8f840a3729537c68d070e0fb3d5f7f6a49
goumin <min.gou@spacemit.com>
2026-01-28 14:28:53 +0800

    Fix memory overlap when loading boot images in eMMC raw-partition boot mode.
    Change-Id: Ia763b53f24f909644253098a84283e0b43ba648c

--- board/spacemit/k3/spl_extra_fit.c
+++ board/spacemit/k3/spl_extra_fit.c
@@ -19,6 +19,7 @@
 #include <part.h>
 #include <fs.h>
 #include <asm/io.h>
+#include <u-boot/md5.h>
 
 #ifdef CONFIG_SPL_MMC
 #include <mmc.h>
@@ -54,7 +55,8 @@ static ulong spl_extra_env_offset(const char *name)
 /* ================= MMC support ================= */
 static ulong spl_extra_mmc_read(struct spl_load_info *load, ulong sector, ulong count, void *buf)
 {
-	struct blk_desc *bd = load->dev;
+	struct mmc *mmc = load->dev;
+	struct blk_desc *bd = mmc_get_blk_desc(mmc);
 	ulong n;
 
 	pr_debug("%s: sector %lx, count %lx, buf %lx\n", __func__, sector, count, (ulong) buf);
@@ -62,41 +64,56 @@ static ulong spl_extra_mmc_read(struct spl_load_info *load, ulong sector, ulong 
 	return n == count ? count : 0;
 }
 
-static int extra_mmc_load_image(struct spl_image_info *spl_image, struct blk_desc *bd,
+static int extra_mmc_load_image(struct spl_image_info *spl_image, struct mmc *mmc,
 				ulong start_lba)
 {
 	struct image_header *header;
 	int err = 0;
+	struct blk_desc *bd = mmc_get_blk_desc(mmc);
 	ulong hdr_cnt = DIV_ROUND_UP(sizeof(*header), bd->blksz);
 
 	header = spl_get_load_buffer(-sizeof(*header), sizeof(*header));
-	if (blk_dread(bd, start_lba, hdr_cnt, header) != hdr_cnt) {
-		pr_err("MMC: read header failed\n");
+	if (blk_dread(bd, start_lba, hdr_cnt, header) != hdr_cnt)
 		return -EIO;
-	}
 
-	if (IS_ENABLED(CONFIG_SPL_LOAD_FIT) && image_get_magic(header) == FDT_MAGIC) {
+	if (IS_ENABLED(CONFIG_SPL_LOAD_FIT) &&
+	    image_get_magic(header) == FDT_MAGIC) {
 		struct spl_load_info load;
-		pr_debug("MMC: found FIT at LBA 0x%lx\n", start_lba);
-		load.dev = bd;
+
+		load.dev = mmc;
 		load.priv = NULL;
 		load.filename = NULL;
 		/* bl_len must be the block size in bytes for FIT loader */
 		load.bl_len = bd->blksz;
 		load.read = spl_extra_mmc_read;
+
 		err = spl_load_simple_fit(spl_image, &load, start_lba, header);
 	} else {
-		pr_err("MMC: unsupported legacy image\n");
 		return -EINVAL;
 	}
+
 	return err;
 }
 
-/* Helper to get an MMC block device based on boot mode */
-static struct blk_desc *get_default_mmc_blk_desc(void)
+static int select_mmc_user_part(struct mmc *mmc)
+{
+	int ret;
+
+	if (CONFIG_IS_ENABLED(MMC_TINY))
+		ret = mmc_switch_part(mmc, 0);
+	else
+		ret = blk_dselect_hwpart(mmc_get_blk_desc(mmc), 0);
+
+	if (ret)
+		pr_debug("MMC: switch to user part failed (%d)\n", ret);
+
+	return ret;
+}
+
+/* Helper to get an MMC device based on boot mode */
+static struct mmc *get_default_mmc_device(void)
 {
 	struct mmc *mmc;
-	struct blk_desc *bd;
 	int mmc_dev_index;
 	enum board_boot_mode boot_mode;
 
@@ -120,16 +137,11 @@ static struct blk_desc *get_default_mmc_blk_desc(void)
 	mmc = find_mmc_device(mmc_dev_index);
 	if (mmc) {
 		if (!mmc_init(mmc)) {
-			bd = mmc_get_blk_desc(mmc);
-			if (bd) {
-				pr_debug("MMC: using device %d based on boot mode 0x%x\n",
-					 mmc_dev_index, boot_mode);
-				return bd;
-			}
+			if (select_mmc_user_part(mmc))
+				return NULL;
+			return mmc;
 		}
 	}
-
-	pr_err("MMC: device %d not available for boot mode 0x%x\n", mmc_dev_index, boot_mode);
 	return NULL;
 }
 
@@ -142,7 +154,8 @@ static int load_fit_from_mmc(struct spl_image_info *caller_spl_image, const char
 {
 	int ret = -1;
 	ulong lba = 0;
-	struct blk_desc *bd = get_default_mmc_blk_desc();
+	struct mmc *mmc = get_default_mmc_device();
+	struct blk_desc *bd = mmc ? mmc_get_blk_desc(mmc) : NULL;
 	struct spl_image_info temp = { 0 };
 
 	if (!bd)
@@ -170,7 +183,7 @@ static int load_fit_from_mmc(struct spl_image_info *caller_spl_image, const char
 			lba = simple_strtoul(image, NULL, 10);
 			pr_debug("MMC: interpret '%s' as LBA=%lu\n", image, lba);
 			pr_debug("MMC: load FIT @ LBA 0x%lx (numeric)\n", lba);
-			ret = extra_mmc_load_image(&temp, bd, lba);
+			ret = extra_mmc_load_image(&temp, mmc, lba);
 		} else {
 			/* treat as partition name and scan */
 			struct disk_partition info;
@@ -190,7 +203,7 @@ static int load_fit_from_mmc(struct spl_image_info *caller_spl_image, const char
 			if (found) {
 				pr_debug("MMC: found partition '%s', start LBA=0x%lx\n", image,
 					 lba);
-				ret = extra_mmc_load_image(&temp, bd, lba);
+				ret = extra_mmc_load_image(&temp, mmc, lba);
 			} else {
 				pr_debug("MMC: partition '%s' not found, will fallback to default "
 					 "LBA\n",
@@ -220,10 +233,11 @@ static int load_fit_from_mmc(struct spl_image_info *caller_spl_image, const char
 	return ret;
 }
 
-static int load_fit_from_mmc_offset(struct spl_image_info *caller_spl_image, ulong offset,
-				    ulong *out_entry)
+static int load_fit_from_mmc_offset(struct spl_image_info *caller_spl_image,
+				    ulong offset, ulong *out_entry)
 {
-	struct blk_desc *bd = get_default_mmc_blk_desc();
+	struct mmc *mmc = get_default_mmc_device();
+	struct blk_desc *bd = mmc ? mmc_get_blk_desc(mmc) : NULL;
 	struct spl_image_info temp = { 0 };
 	ulong lba;
 	int ret;
@@ -232,20 +246,19 @@ static int load_fit_from_mmc_offset(struct spl_image_info *caller_spl_image, ulo
 		return -ENODEV;
 	if (!offset)
 		return -EINVAL;
-	if (offset % bd->blksz) {
-		pr_err("MMC: unaligned offset 0x%lx (blksz 0x%lx)\n",
-		       offset, (ulong)bd->blksz);
+	if (offset % bd->blksz)
 		return -EINVAL;
-	}
 
 	lba = offset / bd->blksz;
-	ret = extra_mmc_load_image(&temp, bd, lba);
+	ret = extra_mmc_load_image(&temp, mmc, lba);
 	if (!ret) {
 		if (temp.fdt_addr)
 			caller_spl_image->fdt_addr = temp.fdt_addr;
+
 		if (out_entry && temp.entry_point)
 			*out_entry = temp.entry_point;
 	}
+
 	return ret;
 }
 #endif /* CONFIG_SPL_MMC */
@@ -593,13 +606,15 @@ static int load_fit_from_mtd(struct spl_image_info *caller_spl_image, const char
  */
 static int load_image_from_mmc_blfs(struct spl_image_info *image, const char *image_path)
 {
+	struct mmc *mmc;
 	struct blk_desc *bd;
 	struct disk_partition info;
 	int ret = -1, part;
 	const char *blfs_name;
 
 	/* Get mmc block device */
-	bd = get_default_mmc_blk_desc();
+	mmc = get_default_mmc_device();
+	bd = mmc ? mmc_get_blk_desc(mmc) : NULL;
 	if (!bd) {
 		pr_err("BLFS: failed to get block device\n");
 		return -ENODEV;
